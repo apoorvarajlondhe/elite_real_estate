cf4a285e20de398e818621ea25a9400917f182c3
apoorvarajlondhe <apoorvarajlondhe15@gmail.com>
2024-01-07 15:00:27 +0530

    initial commit

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./GetStarted.css";
+
+const GetStarted = () => {
+  return (
+    <section className="g-wrapper">
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get started with Homyz</span>
+          <span className="secondaryText">
+            Subscribe and find super attractive price quotes from us. <br />
+            Find your residence soon
+          </span>
+          <div className="button">
+            <a href="mailto:crislearns1@gmail.com">Get Started</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetStarted;
