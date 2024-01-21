637bf71ab0ca4b36e6296e4073b25deb74a830da
apoorvarajlondhe <apoorvarajlondhe15@gmail.com>
2024-01-21 20:13:31 +0530

    removed file paths of header and footer

--- src/pages/Website.jsx
+++ src/pages/Website.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "../components/Header/Header.jsx";
 import Hero from "../components/Hero/Hero.jsx";
 import Companies from "../components/Companies/Companies.jsx";
 import Residencies from "../components/Residencies/Residencies.jsx";
 import Value from "../components/Value/Value.jsx";
 import Contact from "../components/Contact/Contact";
 import GetStarted from "../components/GetStarted/GetStarted.jsx";
-import Footer from "../components/Footer/Footer.jsx";
 
 const Website = () => {
   return (
